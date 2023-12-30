029d4d741366b07c9b06e2ffb0377d0370ea92bd
Benedict Issah <benedictissah@gmail.com>
2023-12-30 12:18:26 -0600

    new changes

--- bank_account.rb
+++ bank_account.rb
@@ -1,16 +1,23 @@
 require_relative "Transactions"
 class Account
-    attr_accessor :balance,:transactions 
+    attr_accessor :balance,:transactions
+
     def initialize(name)
-        @name=name
-        @transactions=[]
-        @balance=0
+
+        @name = name
+
+        @transactions  = []
+
+        @balance = 0
     end
 
 
     def add_transaction(transobj)
+
         @transactions.push(transobj)
-        @balance+=transobj.amount
+
+        @balance += transobj.amount
+
         return   @balance
       
         
--- customer.rb
+++ customer.rb
@@ -2,11 +2,12 @@ require_relative "bank_account"
 require_relative "Transactions"
 class  Customer
     attr_accessor :bank_account,:pin
+
     def initialize(first_name,last_name,pin,bank_account)
-        @first_name=first_name
-        @last_name=last_name
-        @pin=pin
-        @bank_account=Account.new(bank_account)
+        @first_name =  first_name
+        @last_name = last_name
+        @pin = pin
+        @bank_account  =  Account.new(bank_account)
     end
 
 end
